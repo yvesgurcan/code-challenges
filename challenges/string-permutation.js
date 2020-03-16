e4e081583755e59f2a112081e07513e4504bd9cb
yvesgurcan <gurcan.yves+github@gmail.com>
2020-03-15 19:08:17 -0700

    rename function

--- challenges/string-permutation.js
+++ challenges/string-permutation.js
@@ -2,7 +2,22 @@
     Given two strings, write a method to decide if one is a permutation of the other.
 */
 
-function isPerm(text1, text2) {
+// tests
+
+console.log(
+    isPermutation('abc', 'cba') === true ? 'PASS' : 'FAIL',
+    isPermutation('abc', 'cbaa') === false ? 'PASS' : 'FAIL',
+    isPermutation('abc', 'baa') === false ? 'PASS' : 'FAIL',
+    isPermutation('bacc', 'baca') === false ? 'PASS' : 'FAIL',
+    isPermutation('', '') === true ? 'PASS' : 'FAIL',
+    isPermutation('a', 'a') === true ? 'PASS' : 'FAIL',
+    isPermutation('a', 'b') === false ? 'PASS' : 'FAIL',
+    isPermutation('happy', 'hyapp') === true ? 'PASS' : 'FAIL'
+);
+
+// code
+
+function isPermutation(text1, text2) {
     // impossible that they are permutations
     if (text1.length !== text2.length) {
         return false;
@@ -21,16 +36,3 @@ function isPerm(text1, text2) {
 
     return text2_ === '' ? true : false;
 }
-
-// tests
-
-console.log(
-    isPerm('abc', 'cba') === true ? 'PASS' : 'FAIL',
-    isPerm('abc', 'cbaa') === false ? 'PASS' : 'FAIL',
-    isPerm('abc', 'baa') === false ? 'PASS' : 'FAIL',
-    isPerm('bacc', 'baca') === false ? 'PASS' : 'FAIL',
-    isPerm('', '') === true ? 'PASS' : 'FAIL',
-    isPerm('a', 'a') === true ? 'PASS' : 'FAIL',
-    isPerm('a', 'b') === false ? 'PASS' : 'FAIL',
-    isPerm('happy', 'hyapp') === true ? 'PASS' : 'FAIL'
-);

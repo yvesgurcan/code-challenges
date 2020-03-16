b0dc540f1b547d38fd2f4c36500327c942536114
Yves Gurcan <gurcan.yves+github@gmail.com>
2020-03-15 18:08:36 -0700

    Update and rename 1.1.js to unique-characters.js

--- unique-characters.js
+++ unique-characters.js
@@ -1,7 +1,6 @@
 /*
-Is Unique: Implement an algorithm to 
-determine if a string has all unique characters. 
-What if you cannot use additional data structures?
+  Implement an algorithm to 
+  determine if all characters in a string are unique. 
 */
 
 function allUniqueCharsWithSet(input) {

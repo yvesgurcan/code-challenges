e95eea518e8967183d7b883486cd47649ba7ef78
Yves Gurcan <gurcan.yves+github@gmail.com>
2020-03-17 12:18:18 -0700

    Create example1.c

---
+++ big-O/example1.c
@@ -0,0 +1,11 @@
+void foo (int[] array) {
+    int sum = 0;
+    int product = 1;
+    for (int i = 0; i < array.length; i++) {
+        sum += array[i];
+    }
+    for (int i = 0; i < array.length; i++) {
+        product *= array[i];
+    }
+    System.out.println(sum + ", " + product);
+}
